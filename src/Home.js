0d753560cfef311982eac665df03ecd94b02ecd7
Andrew Tran <andrewtran@Andrews-MacBook-Pro.local>
2022-10-15 21:44:28 -0400

    Finished tutorial 5, nesting components

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @returns Homepage
+ */
+const Home = () => {
+    return (
+        <div className="home">
+            <h2>Homepage</h2>
+        </div>
+    );
+}
+
+export default Home;
